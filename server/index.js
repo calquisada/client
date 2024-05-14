362c1c4555889abfb66577559e860a8159f7aa17
calquisada <calquisada@aol.com>
2024-05-14 14:38:18 -0700

    connected to mongoDB

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv").config()
+const cors = require("cors")
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+
+/* Mongoose Setup */
+const PORT = 3001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((err) => console.log(`${err} did not connect`));
+
+
+
